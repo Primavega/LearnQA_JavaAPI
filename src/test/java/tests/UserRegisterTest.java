73babeb8f029d55d9b203dac50f6268563c89637
Primavega <daryakova@gmail.com>
2025-03-01 19:36:15 +0500

    l4

---
+++ src/test/java/tests/UserRegisterTest.java
@@ -0,0 +1,49 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import lib.Assertions;
+import lib.BaseTestCase;
+import lib.DataGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+//лекция 4
+public class UserRegisterTest extends BaseTestCase {
+
+
+
+    @Test
+    public void testCreateUserWithExistingEmail(){
+        String email = "vinkotov@example.com";
+
+        Map<String, String> userData = new HashMap<>();
+        userData.put("email", email);
+        userData = DataGenerator.getRegistrationData(userData);
+
+        Response responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .andReturn();
+
+        Assertions.assertResponseTextEquals(responseCreateAuth, "Users with email '" + email + "' already exists");
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
+    }
+
+    @Test
+    public void testCreateUserSuccessfully(){
+
+        Map<String, String> userData =  DataGenerator.getRegistrationData();
+
+        Response responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .andReturn();
+
+        String[] expectedFields = {"username","firstName","lastName","email"};
+        Assertions.assertJsonHasNotFields(responseCreateAuth, expectedFields);
+    }
+}
